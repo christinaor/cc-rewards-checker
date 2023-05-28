aa95fcb3653efdc1ea0ea81bde29b96867ce8480
Christina Or <christinaor.tech@gmail.com>
2023-05-28 01:26:35 -0400

    Include mock data

---
+++ mockdata/userData.js
@@ -0,0 +1,28 @@
+const userData = [
+  {
+    name: 'Chase Sapphire Preferred',
+    category: 'travel',
+    percentage: null,
+    points: 1,
+  },
+  {
+    name: 'Chase Sapphire Preferred',
+    category: 'dining',
+    percentage: null,
+    points: 3,
+  },
+  {
+    name: 'Capital One Quicksilver',
+    category: 'general',
+    percentage: 1.5,
+    points: null,
+  },
+  {
+    name: 'Capital One SavorOne',
+    category: 'entertainment',
+    percentage: 3.0,
+    points: null,
+  },
+];
+
+export default userData;
