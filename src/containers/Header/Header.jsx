0c84efb013b07f2eb929ed9da3246ffe1fe55da1
Christina Or <christinaor.tech@gmail.com>
2023-06-04 01:47:29 -0400

    Update title

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 export default function Header() {
   return (
     <h1 className={styles.header}>
-      CC Rewards Checker
+      CreditPerks
     </h1>
   )
 }
